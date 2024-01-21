0e22e9d519a4fdf00265eba99c46199f15048dd6
zuhairhk <zuhairhk@gmail.com>
2024-01-21 01:44:22 -0500

    Pathfinding Ultrasonic Testing

--- UTRA_motor_and_sensor.ino
+++ UTRA_motor_and_sensor.ino
@@ -18,8 +18,8 @@
 float leftVal = 0;
 float rightVal = 0;
 float duration, distance;
-unsigned long lastPathDetectionTime = 0;
-const unsigned long TURN_AROUND_INTERVAL = 3000;
+//unsigned long lastPathDetectionTime = 0;
+//const unsigned long TURN_AROUND_INTERVAL = 3000;
 
 void setup() {
   // pinMode(enableA, HIGH);
@@ -87,51 +87,37 @@ void off() {
 void put_it_in_reverse_terry() {
   off();
   left();
+  left();
   // delay(5); // MAYBE CHANGE FOR TURN DISTANCE
 }
 
 void loop() {
-  // digitalWrite(enableA, HIGH);
-  //    forward();
-  //    delay(1000);
-  //    left();
-  //    delay(1000);
-  //    backward();
-  //    delay(1000);
-  //    right();
-  //    delay(1000);
-  //  digitalWrite(green, HIGH);
-  //  digitalWrite(red, HIGH);
-  //    digitalWrite(green, HIGH);
-  //    digitalWrite(red, HIGH);
-  //  //
-  //  //  Serial.println(digitalRead(motorA1));
-  //  //  Serial.println(digitalRead(motorA2));
-  //  Serial.println("zomg!");
-
   Serial.print("Left Val: ");
   Serial.println(leftVal);
   Serial.print("Right Val: ");
   Serial.println(rightVal);
-  //  Serial.print("Digital: ");
-  //  Serial.println(Dig);
-  //delay(100);
-
 
   leftVal = analogRead(leftSensor);
   rightVal = analogRead(rightSensor);
-  //Dig = digitalRead(pinDig);
 
-  //  digitalWrite(trigPin, LOW);
-  //  delayMicroseconds(2);
-  //
-  //  digitalWrite(trigPin, HIGH);
-  //  delayMicroseconds(10);
-  //  digitalWrite(trigPin, LOW);
-  //
-  //  duration = pulseIn(echoPin, HIGH);
-  //
-  //  distance = (duration * 0.0343) / 2; //d=s/t, 0.0343 is speed of sound. 2 because wave travles to and back.
+   digitalWrite(trigPin, LOW);
+   delayMicroseconds(2);
+  
+   digitalWrite(trigPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(trigPin, LOW);
+  
+   duration = pulseIn(echoPin, HIGH);
+  
+   distance = (duration * 0.0343) / 2; //d=s/t, 0.0343 is speed of sound. 2 because wave travles to and back.
+
+  // TURN AROUND (360) IF NO PATH DETECTED FOR MORE THAN 3 SECONDS
+  // if (millis() - lastPathDetectionTime >= TURN_AROUND_INTERVAL) {
+  //   left();
+  //   delay(1000);  // Adjust the delay based on the time needed for a 360-degree turn
+  //   off();
+  //   lastPathDetectionTime = millis();  // Reset the timer after the turn around
+  // }
 
   // TAKE A RIGHT TURN
   if (rightVal >= THRESHOLD && leftVal < THRESHOLD) {
@@ -144,7 +130,7 @@ void loop() {
       delay(10);
     }
     off();
-    lastPathDetectionTime = millis();  // Update the last path detection time
+    //lastPathDetectionTime = millis();  // Update the last path detection time
   }
   // TAKE A LEFT TURN
   else if (leftVal >= THRESHOLD && rightVal < THRESHOLD) {
@@ -157,30 +143,20 @@ void loop() {
       delay(10);
     }
     off();
-    lastPathDetectionTime = millis();  // Update the last path detection time
+    //lastPathDetectionTime = millis();  // Update the last path detection time
   }
   // GO STRAIGHT
   else if (rightVal <= THRESHOLD && leftVal <= THRESHOLD) {
     digitalWrite(green, LOW);
     digitalWrite(red, LOW);
     forward();
-    lastPathDetectionTime = millis();  // Update the last path detection time
-
-    // TURN AROUND (360) IF NO PATH DETECTED FOR MORE THAN 3 SECONDS
-    if (millis() - lastPathDetectionTime >= TURN_AROUND_INTERVAL) {
-      left();
-      delay(1000);  // Adjust the delay based on the time needed for a 360-degree turn
-      off();
-      lastPathDetectionTime = millis();  // Reset the timer after the turn around
-    }
+    //lastPathDetectionTime = millis();  // Update the last path detection time
   }
 
-
-  //  Serial.print("Distance: ");
-  //  Serial.println(distance);
-  //
-  //  if (distance <= 10) {
-  //    put_it_in_reverse_terry();
-  //  }
-  //delay(25);
+   Serial.print("Distance: ");
+   Serial.println(distance);
+  
+   if (distance <= 10) {
+     put_it_in_reverse_terry();
+   }
 }
